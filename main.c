2ac115175a06a586c8bf44a3fa4bf3548eba74ce
anish2454 <shenoy.anish.227@gmail.com>
2017-12-09 21:15:59 -0500

    some fixes

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ char* read_from_story(int size){
 }
 
 int write_to_story(char* line){
-	
+
 	int fd = open("story.txt", O_CREAT|O_APPEND|O_WRONLY, 0777);
 	char s[100];
 	printf("Line: %s\n", line);
@@ -40,23 +40,38 @@ int main(){
 
 	struct sembuf sb;
 	sb.sem_op = -1;
+
+	printf("1\n");
+
 	int semd = semget(SEM, 1, 0777);
 	semop(semd, &sb, 1);
-	int shmd = shmget(SHM, sizeof(int), IPC_CREAT);
+
+  printf("2\n");
+
+	int shmd = shmget(SHM, sizeof(int), 0777|IPC_CREAT);
+
+	printf("2.1  %d\n", shmd);
+
 	int* last_size_p = shmat(shmd, 0, 0);
-	int last_size = *last_size_p;
-	char* last_line = read_from_story(last_size);
+
+	printf("2.3  %d\n", *last_size_p);
+
+	char* last_line = read_from_story(*last_size_p);
+
+	printf("3\n");
+
 	printf("Last Line: %s\n\n", last_line);
 	printf("Enter the next line: ");
 	char next_line[200];
 	fgets(next_line, sizeof(next_line), stdin);
-	last_size = sizeof(next_line);
+	*last_size_p = strlen(next_line);
 	write_to_story(next_line);
-	shmdt(&last_size);
+
+	printf("4\n");
+
+	shmdt(&last_size_p);
+
 	sb.sem_op = 1;
 	semop(semd, &sb, 1);
-	return 0;
-	write_to_story("Hi\n");
-	write_to_story("This is a test\n");
 	return 0;
 }
