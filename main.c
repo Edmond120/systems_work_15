13625022a63a9f97c1b1e4303c0d2ff027baaca9
anish2454 <shenoy.anish.227@gmail.com>
2017-12-09 19:41:17 -0500

    Added Main, not tested, still needs read/write to story

--- main.c
+++ main.c
@@ -7,31 +7,44 @@
 #include <sys/shm.h>
 #include <sys/ipc.h>
 #include <sys/types.h>
-
+#include "keys.h"
+/*
 union semun {
 
 	int val;
 	struct semid_ds *buf;
 	unsigned short *array;
 	struct seminfo *__buf;
-};
+}; */
 
 char* read_from_story(int size){
-	
-	
+
+	return "Hello";
+}
+
+int write_to_story(char* line){
+
+	return 0;
 }
 
 int main(){
 
 	struct sembuf sb;
-	sb.sem_op = -1;	
+	sb.sem_op = -1;
 	int semd = semget(SEM, 1, 0777);
-	semop(sem, &sb, 1);
+	semop(semd, &sb, 1);
 	int shmd = shmget(SHM, sizeof(int), IPC_CREAT);
-	int last_size = *shmat(shmd, 0, 0);
-	//char* last_line = read_from_story(last_size);
-	printf("Last Line: %s\n", last_line);
-	
+	int* last_size_p = shmat(shmd, 0, 0);
+	int last_size = *last_size_p;
+	char* last_line = read_from_story(last_size);
+	printf("Last Line: %s\n\n", last_line);
+	printf("Enter the next line: ");
+	char next_line[200];
+	fgets(next_line, sizeof(next_line), stdin);
+	last_size = sizeof(next_line);
+	write_to_story(next_line);
+	shmdt(&last_size);
+	sb.sem_op = 1;
+	semop(semd, &sb, 1);
+	return 0;
 }
-
-
